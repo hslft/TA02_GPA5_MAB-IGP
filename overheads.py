5ace944129766ee54dc3f93daa4a88daceda7d55
ashleyng24 <ashleyngsiyi@gmail.com>
2022-08-02 17:50:22 +0800

    AHHH

--- overheads.py
+++ overheads.py
@@ -1,5 +1,7 @@
 def overheads_functions(forex):
     """
+    - This function will find the highest overhead category and its value.
+    - This function will return the value in SGD after it is converted using the real-time exchange rate from the API call.
     """
     from pathlib import Path
     import re, csv
@@ -8,6 +10,8 @@ def overheads_functions(forex):
     file_path = file_home/"csv_reports"/"Overheads.csv"
     
     fp_summary = file_home/"summary_report.txt"
+    highestvalue = 0.00
+    Category = ""
     with file_path.open(mode = "r", encoding = "UTF-8") as file:
         reader = csv.reader(file)
         #to skip the header.

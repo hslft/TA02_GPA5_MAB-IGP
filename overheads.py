28bf4d7f44a8e0bbb2946e1dc0fa91be365aa203
ashleyng24 <ashleyngsiyi@gmail.com>
2022-08-02 17:29:58 +0800

    overheads

--- overheads.py
+++ overheads.py
@@ -6,4 +6,19 @@ def overheads_functions(forex):
 
     file_home = Path.cwd()/"project_group"
     file_path = file_home/"csv_reports"/"Overheads.csv"
-    print(file_path.exists())
+    
+    fp_summary = file_home/"summary_report.txt"
+    with file_path.open(mode = "r", encoding = "UTF-8") as file:
+        reader = csv.reader(file)
+        #to skip the header.
+        next(reader)
+        for data in reader:
+            if float(data[1]) > highestvalue:
+                highestvalue = float(data[1])
+                Category = data[0].upper()
+                
+                with fp_summary.open(mode = "w", encoding = "UTF-8", newline = "") as file:
+                    Output = f"\n[HIGHEST OVERHEAD] {Category} : SGD{highestvalue:.2f}"
+                    file.write(Output)
+                    file.close()
+    file.close()

63dbd213069b12b0113b54233e883e45194092a7
hslft <muhdnurejaaz@gmail.com>
2022-08-03 19:11:48 +0800

    change

--- profit_loss.py
+++ profit_loss.py
@@ -5,31 +5,26 @@ def profit_loss_function(forex):
     file_lu = Path.cwd()/"csv_reports"/"Profits and Loss.csv"
     summary = Path.cwd()/"summary_report.txt"
 
-    increase = 0
-    lack = 0
-    PL = []
     with file_lu.open(mode="r",encoding="UTF-8") as file:
         reader = csv.reader(file)
+        DAY = []
+        PL = []
         next(reader)
         for profits in reader:
-            PL.append(profits)
-            count = len(PL)
-        while increase + 1 < count:
-            prev_figure = PL[increase][4]
-            figure = PL[increase + 1][4]
-            PREV_FIGURE = float(prev_figure)
-            FIGURE = float(figure)
-            scenario = PREV_FIGURE > FIGURE
-            if scenario:
-                lack = PREV_FIGURE - FIGURE
-                with summary.open(mode="a",encoding = "UTF-8",newline="") as file:
-                    statement = (f"\n[PROFIT DEFICIT] DAY: {PL[increase + 1][0]}, AMOUNT: SGD{(lack * forex):.2f}")
-                    file.write(statement)
-                    file.close()
-                increase += 1
-        if lack == 0:
+            day = float(profits[0])
+            pl = float(profits[4]) * forex
+            DAY.append(day)
+            PL.append(pl)
+    length = 0
+    for data in range(len(PL) - 1):
+        lack = PL[data] - PL[data + 1]
+        if lack > 0:
             with summary.open(mode="a",encoding = "UTF-8",newline="") as file:
-                statement_2 = (f"\n[PROFIT SURPLUS] NET PROFIT ON EACH DAY IS HIGHER THAN THE PREVIOUS DAY")
-                file.write(statement_2)
-                file.close()
-    file.close()
+                statement1 = (f"\n[PROFIT DEFICIT] DAY: {DAY[data + 1]}, AMOUNT: SGD{lack:.2f}")
+                file.write(statement1)
+            length += 0
+    if length == 0:
+        with summary.open(mode="a",encoding = "UTF-8",newline="") as file:
+            statement2 = (f"\n[PROFIT SURPLUS] NET PROFIT ON EACH DAY IS HIGHER THAN THE PREVIOUS DAY")       
+            file.write(statement2) 
+        

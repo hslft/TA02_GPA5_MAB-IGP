10933f4c4bdb502754073db25630378dd38b9ef1
jerlynchee <jerlynchee222@gmail.com>
2022-08-03 16:38:57 +0800

    ALMOST DONE

--- api.py
+++ api.py
@@ -7,4 +7,14 @@ def api_function():
     summary = Path.cwd()/"project_group"/"summary_report.txt"
 
     url = 'https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=USD&to_currency=SGD&apikey="YTO6ABLT1Z3PLSFS"'
-    
+    response = requests.get(url)
+    data = response.json()
+
+    exchange_rate = data['Realtime Currency Exchange Rate']['5. Exchange Rate']
+    EXCHANGE_RATE = float(exchange_rate)
+
+    with summary.open(mode = "w", encoding = "UTF-8", newline = "") as file:
+        Output = (f"[REAL TIME CURRENCY CONVERSION RATE] USD1 = SGD{EXCHANGE_RATE}")
+        file.write(Output)
+        file.close()
+    return EXCHANGE_RATE

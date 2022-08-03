460bded55538fa8bc093f26026c5a53a0456ba34
jerlynchee <jerlynchee222@gmail.com>
2022-08-03 17:06:44 +0800

    hcdisavbkfyd

--- main.py
+++ main.py
@@ -5,9 +5,10 @@ file_path = house/"project_group"/"summary_report.txt"
 file_path.touch()
 print(file_path)
 print(file_path.exists())
+
 import api, cash_on_hand, overheads, profit_loss
 def main():
-  forex= api.api_function()
+  forex = api.api_function()
   overheads.overheads_function(forex)
   cash_on_hand.coh_function(forex)
   profit_loss.profit_loss_function(forex)

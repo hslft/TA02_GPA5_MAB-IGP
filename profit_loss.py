cc32611f6e305de5d187abfa73b512f7be0b212e
tashatoh <tohtasha@gmail.com>
2022-08-03 13:01:05 +0800

    hi

--- profit_loss.py
+++ profit_loss.py
@@ -1,33 +1,41 @@
-from pathlib import Path
-import csv
-file_path = Path.cwd()/"csv_reports"/"Profit_Loss.csv"
-summary = Path.cwd()/"summary_report.txt"
+# from pathlib import Path
+# import re, csv
+# file_jia = Path.cwd()/"project_group"
+# file_lu = file_jia/"csv_reports"/"Profits and Loss.csv"
+# summary = file_jia/"summary_report.txt"
 def profit_loss_function(): 
+    from pathlib import Path
+    import re, csv
+    
+    file_lu = Path.cwd()/"csv_reports"/"Profits and Loss.csv"
+    summary = Path.cwd()/"summary_report.txt"
+
     increase = 0
     lack = 0
     PL = []
-    with file_path.open(mode="r",encoding="UTF-8",newline="") as file:
+    with file_lu.open(mode="r",encoding="UTF-8") as file:
         reader = csv.reader(file)
         next(reader)
         for profits in reader:
             PL.append(profits)
             count = len(PL)
-            while increase + 1 < count:
-                prev_figure = PL[increase][4]
-                figure = PL[increase + 1][4]
-                PREV_FIGURE = float(prev_figure)
-                FIGURE = float(figure)
-                with summary.open(mode="a",encoding="UTF-8",newline="") as file:
-                    if PREV_FIGURE - FIGURE <  0:
+        while increase + 1 < count:
+            prev_figure = PL[increase][4]
+            figure = PL[increase + 1][4]
+            PREV_FIGURE = float(prev_figure)
+            FIGURE = float(figure)
+            with summary.open(mode="a",encoding="UTF-8",newline="") as file:
+                if PREV_FIGURE - FIGURE <  0:
+                    with summary.open(mode="a",encoding = "UTF-8",newline="") as file:
                         statement = f"\n[PROFIT DEFICIT] DAY: {PL[increase + 1][0]}, AMOUNT: SGD{(lack):.2f}"
                         file.write(statement)
                         file.close()
                         increase += 1
-                        if lack == 0:
-                            with summary.open(mode="a",encoding = "UTF-8",newline="") as file:
-                                statement_2 = f"\n[PROFIT SURPLUS] NET PROFIT ON EACH DAY IS HIGHER THAN THE PREVIOUS DAY"
-                                file.write(statement_2)
-                                file.close()
+        if lack == 0:
+            with summary.open(mode="a",encoding = "UTF-8",newline="") as file:
+                statement_2 = f"\n[PROFIT SURPLUS] NET PROFIT ON EACH DAY IS HIGHER THAN THE PREVIOUS DAY"
+                file.write(statement_2)
+                file.close()
     file.close()
 print(profit_loss_function())
             

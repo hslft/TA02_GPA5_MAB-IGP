c850ff66684d814b546061a517b37612aafc07b3
jerlynchee <jerlynchee222@gmail.com>
2022-08-03 15:55:15 +0800

    api

--- api.py
+++ api.py
@@ -4,4 +4,7 @@ def api_function():
     from pathlib import Path
     import requests
 
-    api_key = "YTO6ABLT1Z3PLSFS"
+    summary = Path.cwd()/"project_group"/"summary_report.txt"
+
+    url = 'https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=USD&to_currency=SGD&apikey="YTO6ABLT1Z3PLSFS"'
+    
